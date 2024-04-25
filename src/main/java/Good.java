c045357d05173c131a7b71d2df9bdeb010f65c22
MrMarkys <ilyasolo8v@gmail.com>
2024-04-25 17:59:31 +0500

    Первый вариант программы

---
+++ src/main/java/Good.java
@@ -0,0 +1,10 @@
+public class Good {
+
+    static String name;
+    static double price;
+
+    Good(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
 
 public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+    static int numberPersons;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        while (true) {
+            System.out.println("Введи количество гостей, на которых нужно разделить счёт");
+
+            if (scanner.hasNextInt()) {
+                numberPersons = scanner.nextInt();
+                scanner.nextLine();
+                if (numberPersons <= 1) {
+                    System.out.println("Ошибка! Количество гостей должно быть больше 1.");
+                } else {
+                    Calculator.count();
+                    break;
+                }
+
+            } else {
+                System.out.println("Ошибка! Некорректное значение");
+                scanner.nextLine();
+            }
+        }
+        scanner.close();
     }
 }
